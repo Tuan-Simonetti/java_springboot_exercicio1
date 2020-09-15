67aa839f420f6d90378f08aec3c057efb9c7e70d
TuanSimonetti <54821029+TuanSimonetti@users.noreply.github.com>
2020-09-15 05:55:18 -0300

    criacao e atualizacao do repositorio

---
+++ src/test/java/com/simonetti/Exercicio01/Exercicio01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonetti.Exercicio01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
